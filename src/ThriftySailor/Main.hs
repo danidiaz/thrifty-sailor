5f9b3a0e4429fb8a6baa077602bbb71c9df45ec8
daniel <daniel@bogusemailserver.com>
2018-02-26 00:22:52 +0100

    options

--- src/ThriftySailor/Main.hs
+++ src/ThriftySailor/Main.hs
@@ -1,10 +1,14 @@
 module ThriftySailor.Main where
 
 import System.Directory
+import Options.Applicative
+
+data Options = Init | Up | Down deriving (Eq,Ord,Show)
 
 defaultMain :: IO ()
 defaultMain = do
     xdg <- getXdgDirectory XdgConfig "thrifty-sailor" 
     print xdg
     return ()
+
 
