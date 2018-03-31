f6b2104cddca1c871aad31d3ce2318a59da26954
daniel <daniel@bogusemailserver.com>
2018-03-31 16:53:37 +0200

    read conf

--- src/ThriftySailor/Main.hs
+++ src/ThriftySailor/Main.hs
@@ -1,15 +1,27 @@
 {-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
 module ThriftySailor.Main where
 
 import System.Directory
+import System.FilePath
+import Control.Exception
+import Data.Aeson
 import Options.Applicative
 import qualified Options.Applicative as O
 
-data Command = Init | Ask | Up | Down deriving (Eq,Ord,Show)
+data Config = Config { doTokenEnvVar :: String } deriving (Eq,Show)
+
+instance FromJSON Config where
+    parseJSON = withObject "Config" $ \v -> Config
+         <$> v .: "DO_TOKEN_ENV_VARIABLE"
+
+data Command = Init | Ask | Up | Down deriving (Eq,Show)
 
 parserInfo :: O.ParserInfo Command
 parserInfo = 
-    let parser = O.subparser . mconcat $
+    let parser = 
+            O.subparser . mconcat $
             [ subcommand (pure Init) $ NameDesc "init" "inits stuff"
             , subcommand (pure Ask) $ NameDesc "ask" "asks stuff"
             , subcommand (pure Up) $ NameDesc "up" "ups stuff"
@@ -27,10 +39,16 @@ subcommand p (NameDesc {name,desc}) = O.command name (infoHelpDesc p desc)
 
 defaultMain :: IO ()
 defaultMain = do
-    xdg <- getXdgDirectory XdgConfig "thrifty-sailor" 
-    print xdg
     command <- O.execParser parserInfo
     print $ command
+    xdg <- getXdgDirectory XdgConfig "thrifty-sailor" 
+    let file = xdg </> "config.json" 
+    print xdg
+    eitherConf <- eitherDecodeFileStrict' file
+    case eitherConf of
+        Left e -> throwIO $ userError e 
+        Right config -> print config
+    print $ "foo"
     return ()
 
 
