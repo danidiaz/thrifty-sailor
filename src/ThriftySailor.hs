c9ea49795c68c036f9f5f1200f168c51eded2a04
daniel <daniel@bogusemailserver.com>
2018-05-13 12:31:52 +0200

    First successful GET request.

--- src/ThriftySailor.hs
+++ src/ThriftySailor.hs
@@ -1,7 +1,9 @@
 module ThriftySailor where
 
-import Data.Aeson
-import Network.Wreq
+import           Data.Aeson
+import           Network.Wreq
+import qualified Data.ByteString.Char8 as Char8
+import           Control.Lens
 
 -- | http://hackage.haskell.org/package/req-1.0.0/docs/Network-HTTP-Req.html
 -- | https://developers.digitalocean.com/documentation/v2/
@@ -13,6 +15,11 @@ baseUrl :: String
 baseUrl = "https://api.digitalocean.com"
 
 listAllDroplets :: Token -> IO Value
-listAllDroplets = undefined
+listAllDroplets token =
+    let opts = set auth (Just (oauth2Bearer (Char8.pack token))) defaults
+        relpath = "/v2/droplets"
+     in do r <- getWith opts (baseUrl ++ relpath)
+           r' <- asValue r
+           return $ view responseBody r'
 
 
