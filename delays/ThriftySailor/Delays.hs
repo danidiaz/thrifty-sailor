c5f25555e3ad88685e28f13b5094716590ea2b05
daniel <daniel@bogusemailserver.com>
2018-05-27 15:12:46 +0200

    ...

--- delays/ThriftySailor/Delays.hs
+++ delays/ThriftySailor/Delays.hs
@@ -1,6 +1,16 @@
-module ThriftySailor.Delays () where
+{-# LANGUAGE NumDecimals #-}
+module ThriftySailor.Delays (
+     Millis
+   , millis
+) where
 
+import           Control.Concurrent
 import           Streaming
 import qualified Streaming.Prelude as S
+
+newtype Millis = Millis { getMillis :: Int } deriving (Eq,Ord,Show)
+
+millis :: Int -> Millis
+millis = Millis
 
 
